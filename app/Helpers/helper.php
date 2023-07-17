17e9f35509626c1fb0a6be3cd6667b9739c83ccb
Alok Das <alokdas4all@gmail.com>
2023-07-17 21:50:05 +0530

    Update helper.php
    Added the php indian currency format in helper

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -280,3 +280,32 @@ function getApplicationId($exporter_id)
 {
     return \App\Models\tbl_application_details::select('id')->where('exporter_id', $exporter_id)->first()->id;
 }
+
+function IND_money_format($number){
+    $decimal = (string)($number - floor($number));
+    $money = floor($number);
+    $length = strlen($money);
+    $delimiter = '';
+    $money = strrev($money);
+
+    for($i=0;$i<$length;$i++){
+        if(( $i==3 || ($i>3 && ($i-1)%2==0) )&& $i!=$length){
+            $delimiter .=',';
+        }
+        $delimiter .=$money[$i];
+    }
+
+    $result = strrev($delimiter);
+    $decimal = preg_replace("/0\./i", ".", $decimal);
+    $decimal = substr($decimal, 0, 3);
+
+    if( $decimal != '0'){
+        $result = $result.$decimal;
+    }
+
+    return $result;
+}
+
+// Usage
+// $price = IND_money_format(500000000.26);
+//  echo $price;
